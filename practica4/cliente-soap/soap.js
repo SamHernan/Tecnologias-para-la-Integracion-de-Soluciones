75cab5127aea229d73e34f0c5f45697aa1ce06eb
SamHernan <71732378+SamHernan@users.noreply.github.com>
2021-03-23 12:48:54 -0600

    Cambios finales en el cliente

--- practica4/cliente-soap/soap.js
+++ practica4/cliente-soap/soap.js
@@ -1,22 +1,33 @@
-var valor1 = document.getElementById("a").value;
-var valor2 = document.getElementById("b").value;
-var mensaje = '<?xml version="1.0" encoding="utf-8"?>' +
+var mensaje = '';
+function ini(){
+    mensaje = '<?xml version="1.0" encoding="utf-8"?>' +
     '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
     '<Body>' +
     '<SumarRequest xmlns="http://www.example.org/calculadora">' +
-    '<a>valor1</a>' +
-    '<b>valor2</b>' +
+    '<a>'+ document.getElementById('a').value +'</a>' +
+    '<b>'+ document.getElementById('b').value +'</b>' +
     '</SumarRequest>' +
     '</Body>' +
     '</Envelope>';
-
+}
 function soap() {
     // alert('hola')
+    ini();
     axios.post('http://localhost:8081/ws/calculadora', mensaje,{
         headers:{
             'Content-Type' : 'text/xml'
         }
     })
-    .then(response => console.log(response.data))
+    .then(function(response){
+        //console.log(response.data)
+        document.getElementById('r').value = resultado(response.data)
+    })
     .catch(err => console.log(err));
+}
+
+function resultado(rXml){
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(rXml, "text/xml");
+    var resul = xmlDoc.getElementsByTagName("ns2:resultado")[0].childNodes[0].nodeValue;
+    return resul;
 }
